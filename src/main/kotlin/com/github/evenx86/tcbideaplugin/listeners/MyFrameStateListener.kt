21b3fe94288d6891da98c1b3e4b9d0e805c5306c
GitHub Action <action@github.com>
2023-03-25 08:56:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/evenx86/tcbideaplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/evenx86/tcbideaplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.evenx86.tcbideaplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
